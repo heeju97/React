b7589f5ed1e386e0c0fc94b51e72354baa78923f
희주 <149589749+heeju97@users.noreply.github.com>
2024-01-29 17:02:27 +0900

    React
    Online shopping mall web page created using React!
    Using React Bootstrap(navbar, container)

--- App.js
+++ App.js
@@ -1,23 +1,68 @@
+/* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from "react";
+import {Button, Navbar, Container, Nav, Col ,Row} from 'react-bootstrap';
+import img1 from './con1.jpeg';
+import img2 from './con2.jpeg';
+import img3 from './con3.jpeg';
+import data from './data.js';
+
+
+
 
 function App() {
+
+let [products] = useState(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home" className='homeLogo'>HaniCHU</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className='menuText'>Home</Nav.Link>
+            <Nav.Link href="#features" className='menuText'>Item</Nav.Link>
+            <Nav.Link href="#pricing" className='menuText'>Sale</Nav.Link>
+            <Nav.Link href="#pricing" className='menuText'>More</Nav.Link>
+            <Nav.Link href="#pricing" className='menuText'>Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg'></div>
+      <br/>
+      
+      {/* <Container> */}
+        <Row>
+          <Col>
+            <div className='container-img'>
+              <img src={img1}></img>
+              <h4>{products[0].title}</h4>
+              <p>{products[0].content}</p>
+              <p>{products[0].price}원</p>
+            </div>
+          </Col>
+          <Col>
+            <div className='container-img'>
+              <img src={img2} ></img>
+              <h4>{products[1].title}</h4>
+              <p>{products[1].content}</p>
+              <p>{products[1].price}원</p>
+            </div>
+          </Col>
+          <Col>
+            <div className='container-img'>
+            <img src={img3} ></img>
+              <h4>{products[2].title}</h4>
+              <p>{products[2].content}</p>
+              <p>{products[2].price}원</p>
+            </div>
+          </Col>
+        </Row>
+    {/* </Container> */}
     </div>
   );
 }
---
+++ data.js
@@ -0,0 +1,29 @@
+// 길고 복잡한 변수 보관
+// 함수, 컴포넌트, 변수 등 가능
+
+
+
+
+let data = [
+{
+    id:0,
+    title: 'product1',
+    content: '어쩌구1',
+    price: 15900
+},
+{
+    id:1,
+    title: 'product2',
+    content: '어쩌구2',
+    price: 25900
+},
+{
+    id:2,
+    title: 'product3',
+    content: '어쩌구3',
+    price: 26300
+}
+
+]
+
+export default data ;
