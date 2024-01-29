959e84e23f03ebf9f3eae7ccc8ebc485d66c3b71
희주 <149589749+heeju97@users.noreply.github.com>
2024-01-29 17:10:35 +0900

    React
    creating a blog with a posting function using React
    onClick event Handler=> plus++, posting, Modal etc..

--- App.js
+++ App.js
@@ -1,70 +1,163 @@
-/* eslint-disable */
+/*eslint-disable */
+// ⬆️warning messages 없애는 방법
+
 import logo from './logo.svg';
 import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import {useState} from "react";
-import {Button, Navbar, Container, Nav, Col ,Row} from 'react-bootstrap';
-import img1 from './con1.jpeg';
-import img2 from './con2.jpeg';
-import img3 from './con3.jpeg';
-import data from './data.js';
+import { useState } from 'react';
+import * as React from 'react' 
 
 
 
 
 function App() {
+ 
+  // let post = '강남 우동 맛집'; 
+  // let [글제목, b] = useState('맛집 추천');
+  let [logo, setLogo] = useState('React BLOG');
 
-let [products] = useState(data)
+  let [글제목, title] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
+  let [좋아요,plus] = useState([0,0,0]);
+  // let [좋아요,plus] = useState(0);
+  // let [좋아요,plus] = useState(0);
+  let [modal, setModal] = useState(false);
+  let [modalTitle, setMT] = useState(0);
+  let [입력값,입력값변경]= useState('');
 
+  // function plus (){
+  //   console.log(1);
+  // }
+ 
+ 
   return (
     <div className="App">
-  
-    <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="#home" className='homeLogo'>HaniCHU</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home" className='menuText'>Home</Nav.Link>
-            <Nav.Link href="#features" className='menuText'>Item</Nav.Link>
-            <Nav.Link href="#pricing" className='menuText'>Sale</Nav.Link>
-            <Nav.Link href="#pricing" className='menuText'>More</Nav.Link>
-            <Nav.Link href="#pricing" className='menuText'>Cart</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <div className="black-nav">
+       <h4>{logo}</h4>
+      </div>
 
-      <div className='main-bg'></div>
-      <br/>
+    
+      <button onClick={()=>{
+        let sortt = [...글제목];
+        sortt.sort();
+        title(sortt)
+      }}>🔤</button>
       
-      {/* <Container> */}
-        <Row>
-          <Col>
-            <div className='container-img'>
-              <img src={img1}></img>
-              <h4>{products[0].title}</h4>
-              <p>{products[0].content}</p>
-              <p>{products[0].price}원</p>
-            </div>
-          </Col>
-          <Col>
-            <div className='container-img'>
-              <img src={img2} ></img>
-              <h4>{products[1].title}</h4>
-              <p>{products[1].content}</p>
-              <p>{products[1].price}원</p>
-            </div>
-          </Col>
-          <Col>
-            <div className='container-img'>
-            <img src={img3} ></img>
-              <h4>{products[2].title}</h4>
-              <p>{products[2].content}</p>
-              <p>{products[2].price}원</p>
-            </div>
-          </Col>
-        </Row>
-    {/* </Container> */}
+      <button onClick={()=>{
+        // 원본 데이터를 수정하는 방식⬇️
+        // 글제목[0] ='여자 코트 추천';
+        // title(글제목);
+
+        //원본 데이터 수정x ,카피본 만들기 (array object 다룰 때는 원본을 보존하는 것이 좋음)
+        let copy = [...글제목];
+        copy[0]='여자 코트 추천';
+        title(copy);
+
+      //[... ]=> shallow copy or deep copy => 메모리 주소가 변경됨
+      //같은 arr를 복사해 만들면, 같은 state로 인식되어 작동하지 않음=> shallow copy를 사용해 새로운 메모리 주소를 받아 다른 arr로 인식 가능
+
+      }}>👚</button>
+
+      {/* <div className="list"> 
+        <h4>{ 글제목[0] } <span onClick={()=>{ plus(좋아요++)}}>👍</span> {좋아요} </h4>
+        <p>1월 24일</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>1월 24일</p>
+      </div>
+      <div className="list">
+        <h4 onClick={()=> {setModal(!modal)}}>{ 글제목[2] }</h4>
+        <p>1월 24일</p>
+      </div> */}
+
+      {
+        글제목.map(function(a,i){
+          return (<div className="list">
+          <h4 onClick={()=> {setModal(!modal); setMT(i)}}>{ a }<span onClick={(e)=>{
+            e.stopPropagation();
+            let copyy = [...좋아요];
+            copyy[i] = copyy[i]+1;
+            plus(copyy)
+          }}>👍</span> {좋아요[i]} </h4>
+          <p>1월 24일</p>
+          <button className='postbtn' onClick={()=>{
+            let copy = [...글제목];
+            copy.splice(i,1);
+            title(copy);
+          }}>❌</button>
+        </div>)
+        })
+      }
+
+
+      <input onChange={(e)=>{입력값변경(e.target.value);
+      console.log(입력값)}}></input>
+      <button onClick={()=>{
+        //내 코드
+        // input값을 추출
+          // console.log(입력값)
+        // 추출된 input 값을 글제목 array에 요소로 담기 =unshift 사용? 
+        // 글제목.unshift(입력값)
+      //강의코드
+      let copy = [...글제목];
+
+      copy.unshift(입력값);
+      title(copy);
+
+      }}>➕포스팅</button>
+
+
+      {/* <Modal></Modal> */}
+      
+
+        {
+          // html 중간에서 js(if, for) 등을 사용할 수 없음 => 삼항연산자(ternary operator) 사용
+          // 조건식 ? 참일 경우 실행할 코드 : 거짓을 경우 실행할 코드
+          modal == true ? <Modal 글제목={글제목} modalTitle={modalTitle}/> : ''
+          
+        }
+        <Modal1234></Modal1234>
+  
     </div>
   );
+}
+
+
+function Modal(props){
+  
+  return(
+    <div className="modal">
+        <h4>{
+          props.글제목[props.modalTitle]
+        }</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+        <button onClick={()=>{
+          // let coppy = [...props.글제목];
+          // coppy[0] = '여자 코트 추천';
+          // props.title(coppy);
+        }}>수정</button>
+    </div>
+  )
+
+}
+
+class Modal1234 extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      name : 'han',
+      age : 20
+    }
+  }
+  render(){
+    return(
+      <div>안녕{this.state.age}
+      <button onClick={()=>{
+        this.setState({name: 'kim'})
+      }}>💥</button>
+      </div>
+    )
+  }
 }
 
 export default App;
